8ddf082580449288f9aff72a8cdd7ef662e4f241
Simon Pasi <ssp.pasi@gmail.com>
2024-07-16 12:17:01 +0200

    3D • Add multiple renders

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import ThreeCube from './ThreeCube';
-import SketchfabEmbed from './SketchfabEmbed';
+import React, { useMemo } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { clone } from 'three/examples/jsm/utils/SkeletonUtils';
+
+// Get model as a copy to allow multiple renders of the same model.
+function Model({ path }) {
+  const { scene } = useGLTF(path);
+  const clonedScene = useMemo(() => clone(scene), [scene]);
+  return <primitive object={clonedScene} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      {/*<ThreeCube />*/}
-      <h1>My Sketchfab Embed</h1>
-      <SketchfabEmbed />
+    // Show multiple canvases.
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <div style={{ flex: 1 }}>
+        <Canvas camera={{ position: [0, 0, 2] }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[2, 2, 2]} />
+          <OrbitControls />
+          <Model path="/model.glb" />
+        </Canvas>
+      </div>
+      <div style={{ flex: 1 }}>
+        <Canvas camera={{ position: [0, 0, 2] }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[2, 2, 2]} />
+          <OrbitControls />
+          <Model path="/model.glb" />
+        </Canvas>
+      </div>
     </div>
   );
 }
